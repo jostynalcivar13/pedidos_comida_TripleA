8268770954b87b9498c2ef6df0184f41b9b2eb7a
jostynalcivar13 <jostynalcivar1234@gmail.com>
2025-07-19 10:48:39 -0500

    docker completo

---
+++ README.md
@@ -0,0 +1,9 @@
+Grupo de trabajo:
+
+- Alcivar Fuentes Hansel Adrian
+- Alcivar Montesdeoca Jostyn Orlando
+- Aspiazu Sánchez Arianna Misaely
+
+README Grupo 2 · Pedidos de Comida Rápida
+
+API's
--- app/models/connection.py
+++ app/models/connection.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 #cambiar a localhost si se usa en local
- #client = MongoClient("mongodb://localhost:27017/")
+#client = MongoClient("mongodb://localhost:27017/")
 
 #cambiar a mongo si se usa en docker
 client = MongoClient("mongodb://mongo:27017/")
